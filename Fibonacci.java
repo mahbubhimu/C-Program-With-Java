797744892236fa568645a7808f9c4b0fb28b825c
Mahbubur Rahman Chowdhury <mahbubhimu@gmail.com>
2018-09-17 11:42:36 +0600

    Update Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -19,11 +19,12 @@ class Fibonacci{
         System.out.println();
     }
 }
-//By default in a fibonacci series the First and Second number will be set either 0,1 or 1,1. Rest of the 
-//number will be produce from these two numbers. 
+
 public class JavaApplication3 {
     public static void main(String[] args) {
         Fibonacci objFib = new Fibonacci();
         objFib.printFibonacci();
     }
 }
+/*Note : By default in a fibonacci series the First and Second number will be set either 0,1 or 1,1. Rest of the 
+number will be produce from these two numbers. */
